81680452c4f1723b116cd49086b2d0354dcd06da
Rukai <rubickent@gmail.com>
2014-11-15 15:47:47 +1100

    Added client side check for file size.

--- js/login.js
+++ js/login.js
@@ -41,26 +41,3 @@ function validatePassword(){
     }
 }
 
-/* Takes a message element and gives a success message with appropriate styling*/
-function successMessage(messageElement){
-    messageElement.style.visibility = "visible";
-    messageElement.innerHTML = '<span class="glyphicon glyphicon-ok"></span> All Good!';
-    messageElement.classList.remove("text-warning");
-    messageElement.classList.add("text-success");
-
-    parrentElement = messageElement.parentNode;
-    parrentElement.classList.remove("has-warning");
-    parrentElement.classList.add("has-success");
-}
-
-/* Takes a message element and warning message, styling it appropiately*/
-function warningMessage(messageElement, messageString){
-    messageElement.innerHTML = '<span class="glyphicon glyphicon-warning-sign"></span> ' + messageString
-    messageElement.style.visibility = "visible";
-    messageElement.classList.remove("text-success");
-    messageElement.classList.add("text-warning");
-
-    parrentElement = messageElement.parentNode;
-    parrentElement.classList.remove("has-success");
-    parrentElement.classList.add("has-warning");
-}
--- js/newFile.js
+++ js/newFile.js
@@ -1,12 +1,45 @@
+//Calls all validation methods to ensure new file form is valid
 function validateForm(){
-    //Get date from datetime picker and store it in a hidden field
+    return validateDate() && setDateSubmit() && validateFile() && validateComment();
+}
+
+//Get date from datetime picker and store it in a hidden field
+function setDateSubmit(){
     unixTime = $("#datetimeInput").data("DateTimePicker").getDate().unix();
     $("#datetimeSubmit").val(unixTime);
     return true;
 }
 
+//Ensures date chosen by the user is valid
+function validateDate(){
+    return true;
+}
+
+//Ensures file chosen by the user is valid
+function validateFile(){
+    file = $('[name="file"]')[0];
+    filename = file.value;
+    //get size in MB
+    size = (file.size / 1024) / 1024;
+    
+    if (filename == ""){//No file selected
+        return false;
+    }
+    else if(size > 10){//file > 10 MB
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+//Ensures comment chosen by the user is valid
+function validateComment(){
+    return true;
+}
+
+//activates date time picker
 $(function () {
-    //activates date time picker
     $('#datetimeInput').datetimepicker();
     $("#datetimeInput").data("DateTimePicker").show();
 });
